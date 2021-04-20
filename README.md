ffecb16b0f21bf6037a6b02c0ef5e0059cf0127d
majesty101 <jody.harrison32@gmail.com>
2021-04-19 20:55:27 -0500

    starter

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Info3180-Project2
+# Lab 6 - VueJS
+
+The goal of this lab is to become familiar with VueJS.
+
+Remember to always create a virtual environment and install the packages in your requirements file
+
+```bash
+$ python -m venv venv (you may need to use python3 instead)
+$ source venv/bin/activate (or .\venv\Scripts\activate on Windows)
+$ pip install -r requirements.txt 
+$ python run.py
+```
---
+++ app/static/js/app.js
@@ -0,0 +1,54 @@
+/* Add your Application JavaScript */
+const app = Vue.createApp({
+  data() {
+    return {
+      welcome: 'Hello World! Welcome to VueJS'
+    }
+  }
+});
+
+app.component('app-header', {
+  name: 'AppHeader',
+  template: `
+      <header>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+            <a class="navbar-brand" href="#">VueJS App</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">News</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+      </header>    
+  `,
+  data: function() {
+    return {};
+  }
+});
+
+app.component('app-footer', {
+  name: 'AppFooter',
+  template: `
+      <footer>
+          <div class="container">
+              <p>Copyright &copy {{ year }} Flask Inc.</p>
+          </div>
+      </footer>
+  `,
+  data: function() {
+      return {
+          year: (new Date).getFullYear()
+      }
+  }
+})
+
+app.mount('#app');
