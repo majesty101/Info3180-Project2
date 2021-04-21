4ebca8593ba0424b6a3bc6296da3a6ae2ca22e07
majesty101 <56702010+majesty101@users.noreply.github.com>
2021-04-21 00:38:09 -0500

    updated Config

--- app/config.py
+++ app/config.py
@@ -4,8 +4,8 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://project2:project2@localhost/project2'
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://project_2:project_2@localhost/project_2'
+    UPLOAD_FOLDER = './uploads'                                                             
 class DevelopmentConfig(Config):
     """Development Config that extends the Base Config Object"""
     DEVELOPMENT = True
